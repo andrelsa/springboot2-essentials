4188210a133a249840f244e9f092654ae7850dfb
SP01\al.soares <al.soares@totvs.com.br>
2020-12-21 17:17:56 -0300

    Spring security - CSRF TOKEN

--- src/main/java/academy/devdojo/springboot/config/SecurityConfig.java
+++ src/main/java/academy/devdojo/springboot/config/SecurityConfig.java
@@ -14,7 +14,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable()
+//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .and()
+                .authorizeRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
